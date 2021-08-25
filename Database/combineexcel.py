8b5d3c024592143dbfee10606c0c1a0a8a214af4
Prithvi Kumar <prithvikumar@hotmail.com>
2021-08-25 22:00:37 +0530

    normalised nomenclature, modified 2018

--- Database/combineexcel.py
+++ Database/combineexcel.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-workbook_url = 'Water_Quality_Rivers_2016.xlsx'
-excel_file = pd.read_excel(workbook_url, sheet_name=None)
+workbook_url = 'Water_quality_rivers_2018.xlsx'
+excel_file = pd.read_excel(workbook_url, engine='openpyxl', sheet_name=None)
 df = pd.concat(excel_file.values())
 df.to_excel("output.xlsx")
-print(df)
