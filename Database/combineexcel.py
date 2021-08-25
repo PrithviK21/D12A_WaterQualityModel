1bb2414390b72b84690b38302d35857b322f5726
Anurag Saraswat <anu01saraswat@gmail.com>
2021-08-25 20:57:31 +0530

    Merged 2016

--- Database/combineexcel.py
+++ Database/combineexcel.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-workbook_url = 'Water_Quality_Rivers_2017.xlsx'
+workbook_url = 'Water_Quality_Rivers_2016.xlsx'
 excel_file = pd.read_excel(workbook_url, sheet_name=None)
 df = pd.concat(excel_file.values())
 df.to_excel("output.xlsx")
