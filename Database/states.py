66d54d4c4aefcd83b38a8bad016fb43e8e27a613
Prithvi Kumar <prithvikumar@hotmail.com>
2021-08-30 14:29:59 +0530

    added 2007, added states to station code xlsx

--- Database/states.py
+++ Database/states.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-excel_file = "water_quality_rivers_2010.xlsx"
+excel_file = "water_quality_rivers_2007.xlsx"
 
-shubham_df = pd.read_excel("water_quality_rivers_2012.xlsx")
+shubham_df = pd.read_excel("station_codes_with_states.xlsx")
 df = pd.read_excel(excel_file)
 
 for ind in df.index:
     # checks if current station code exists and gets the particular row
     try:
-        df['States'][ind] = shubham_df[shubham_df['STATION CODE'] == df['STATION CODE'][ind]]['States'].values[0]
+        df['STATE'][ind] = shubham_df[shubham_df['STATION CODE'] == df['STATION CODE'][ind]]['STATE'].values[0]
     except:
-        df['States'][ind] = -1
+        df['STATE'][ind] = -1
  
-# print(df.States)
+# print(df.STATE)
 df.to_excel("output.xlsx")
