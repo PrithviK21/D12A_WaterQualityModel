38815661c1399dae0b278dec7e274571303f59b3
Prithvi Kumar <prithvikumar@hotmail.com>
2021-08-30 12:07:27 +0530

    normalised headers, added station_codes.py to be checked by other members, created To Do folder for remaining data

---
+++ Database/station_codes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import glob
+
+filenames = glob.glob("*.xlsx")
+bruh = [pd.read_excel(filename, index_col=None) for filename in filenames]
+needed = ['STATION CODE', 'LOCATION']
+station_df = pd.DataFrame(columns=needed)
+print(station_df.head())
+for df in bruh:
+    print(df[needed].head())
+    pd.concat([station_df, df[needed]])
+    print(station_df.head())
