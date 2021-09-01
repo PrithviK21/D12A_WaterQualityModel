dd3dc915ce15eae6779c3faee35d34c8057ee186
Prithvi Kumar <prithvikumar@hotmail.com>
2021-09-01 16:06:11 +0530

    finally fixed 2018, optimised mean.py

--- Database/meanwaterquality.py
+++ Database/meanwaterquality.py
@@ -1,22 +1,20 @@
 import pandas as pd
-df = pd.read_excel("Water_Quality_Rivers_2016.xlsx")
-for i in df.index:
-    try:
-        df['Temperature mean'][i] = float((df['Temperature min'][i] + df['Temperature max'][i])/2)
-        df['Dissolved Oxygen mean'][i] = float((df['Dissolved Oxygen min'][i] + df['Dissolved Oxygen max'][i])/2)
-        df['pH mean'][i] = float((df['pH min'][i] + df['pH max'][i])/2)
-        df['Conductivity (µmho/cm) mean'][i] = float((df['Conductivity (µmho/cm) min'][i] + df['Conductivity (µmho/cm) max'][i])/2)
-        df['Bio- Chemical Oxygen Demand (mg/L) mean'][i] = float((df['Bio- Chemical Oxygen Demand (mg/L) min'][i] + df['Bio- Chemical Oxygen Demand (mg/L) max'][i])/2)
-        df['Nitrate mean'][i] = float((df['Nitrate min'][i] + df['Nitrate max'][i])/2)
-        df['Faecal Coliform mean'][i] = float((df['Faecal Coliform min'][i] + df['Faecal Coliform max'][i])/2)
-        df['Total Coliform mean'][i] = float((df['Total Coliform min'][i] + df['Total Coliform max'][i])/2)
-    except:
-        df['Temperature mean'][i] = -1
-        df['Dissolved Oxygen mean'][i] = -1
-        df['pH mean'][i] = -1
-        df['Conductivity (µmho/cm) mean'][i] = -1
-        df['Bio- Chemical Oxygen Demand (mg/L) mean'][i] = -1
-        df['Nitrate mean'][i] = -1
-        df['Faecal Coliform mean'][i] = -1
-        df['Total Coliform mean'][i] = -1
+df = pd.read_excel("Water_Quality_Rivers_2018.xlsx")
+# for i in df.index:
+#         df['TEMPERATURE MEAN'][i] = (int(df['TEMPERATURE MIN'][i]) + int(df['TEMPERATURE MAX'][i]))/2
+#         df['DISSOLVED OXYGEN MEAN'][i] = (int(df['DISSOLVED OXYGEN MIN'][i]) + int(df['DISSOLVED OXYGEN MAX'][i]))/2
+#         df['PH MEAN'][i] = (int(df['PH MIN'][i]) + int(df['PH MAX'][i]))/2
+#         df['CONDUCTIVITY (ΜMHO/CM) MEAN'][i] = (int(df['CONDUCTIVITY (ΜMHO/CM) MIN'][i]) + int(df['CONDUCTIVITY (ΜMHO/CM) MAX'][i]))/2
+#         df['BIO- CHEMICAL OXYGEN DEMAND (MG/L) MEAN'][i] = (int(df['BIO- CHEMICAL OXYGEN DEMAND (MG/L) MIN'][i]) + int(df['BIO- CHEMICAL OXYGEN DEMAND (MG/L) MAX'][i]))/2
+#         df['NITRATE MEAN'][i] = (int(df['NITRATE MIN'][i]) + int(df['NITRATE MAX'][i]))/2
+#         df['FAECAL COLIFORM MEAN'][i] = (int(df['FAECAL COLIFORM MIN'][i]) + int(df['FAECAL COLIFORM MAX'][i]))/2
+#         df['TOTAL COLIFORM MEAN'][i] = (int(df['TOTAL COLIFORM MIN'][i]) + int(df['TOTAL COLIFORM MAX'][i]))/2
+df['TEMPERATURE MEAN'] = (df['TEMPERATURE MIN'] + df['TEMPERATURE MAX'])/2
+df['DISSOLVED OXYGEN MEAN'] = (df['DISSOLVED OXYGEN MIN'] + df['DISSOLVED OXYGEN MAX'])/2
+df['PH MEAN'] = (df['PH MIN'] + df['PH MAX'])/2
+df['CONDUCTIVITY (ΜMHO/CM) MEAN'] = (df['CONDUCTIVITY (ΜMHO/CM) MIN'] + df['CONDUCTIVITY (ΜMHO/CM) MAX'])/2
+df['BIO- CHEMICAL OXYGEN DEMAND (MG/L) MEAN'] = (df['BIO- CHEMICAL OXYGEN DEMAND (MG/L) MIN'] + df['BIO- CHEMICAL OXYGEN DEMAND (MG/L) MAX'])/2
+df['NITRATE MEAN'] = (df['NITRATE MIN'] + df['NITRATE MAX'])/2
+df['FAECAL COLIFORM MEAN'] = (df['FAECAL COLIFORM MIN'] + df['FAECAL COLIFORM MAX'])/2
+df['TOTAL COLIFORM MEAN'] = (df['TOTAL COLIFORM MIN'] + df['TOTAL COLIFORM MAX'])/2
 df.to_excel("output16.xlsx")
