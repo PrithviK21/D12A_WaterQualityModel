ebd6e5697fcbb1e81e6f42bbc6673c99682f52e6
Shubham-Zope <2019shubham.zope@ves.ac.in>
2022-02-28 17:28:03 +0530

    Added tabs in line page and styled compare

--- Frontend/wqi-frontend/src/pages/LineChart.jsx
+++ Frontend/wqi-frontend/src/pages/LineChart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import SelectSearch, { fuzzySearch } from "react-select-search";
 import "../select-search.css";
 import rivers from "../rivers";
@@ -11,11 +11,12 @@ const Plotly = window.Plotly;
 const Plot = createPlotlyComponent(Plotly);
 
 function LineChart() {
+  const [activeTab, setActiveTab] = useState("Individual");
   const [riverData, setRiverData] = useState(null);
   const [river, setRiver] = useState("ALKANANDA");
   const [firstriver, setFirstRiver] = useState("");
   const [secondriver, setSecondRiver] = useState("");
-  const [comparedata, setCompareData] = useState([]);
+  const [comparedata, setCompareData] = useState(null);
   const options = rivers;
 
   const handleChange = (val) => {
@@ -77,105 +78,155 @@ function LineChart() {
       });
   }, [river]);
 
-  // useEffect(() => {
-  //   const endpoint =
-  //     "http://127.0.0.1:8000/compareapi/?comparerivername=" +
-  //     firstriver +
-  //     "," +
-  //     secondriver;
-  //   axios
-  //     .get(endpoint)
-  //     .then((response) => {
-  //       const comdata = response.data;
-  //       setCompareData(comdata);
-  //       console.log(comdata);
-  //     })
-  //     .catch((e) => {
-  //       console.log(e);
-  //     });
-  // }, [firstriver, secondriver]);
+  useEffect(() => {
+    const endpoint =
+      "http://127.0.0.1:8000/compareapi/?comparerivername=" +
+      firstriver +
+      "," +
+      secondriver;
+    axios
+      .get(endpoint)
+      .then((response) => {
+        const comdata = response.data;
+        setCompareData(comdata);
+        console.log(comdata);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [firstriver, secondriver]);
+
+  const handleTab1 = () => {
+    // update the state to tab1
+    setActiveTab("Individual");
+  };
+  const handleTab2 = () => {
+    // update the state to tab2
+    setActiveTab("Compare");
+  };
 
   return (
     <div>
-      <Container className="map-container">
-        <Row xs={1}>
-          <Col>
-            <SelectSearch
-              options={options}
-              placeholder="Choose your river"
-              search
-              filterOptions={fuzzySearch}
-              onChange={handleChange}
-              value={river}
-            />
-          </Col>
-        </Row>
-        <Row>
-          {riverData ? (
-            <div className="graph-placeholder" id="graphplaceholder">
-              <Plot
-                data={[riverData.trace1, riverData.trace2]}
-                layout={{
-                  title: { text: `River ${river}` },
-                  autosize: true,
-                  xaxis: {
-                    title: "Year",
-                  },
-                  yaxis: {
-                    title: "WQI",
-                  },
-                }}
-                useResizeHandler
-                style={{ width: "100%", height: "100%" }}
-              />
-            </div>
-          ) : (
-            <div className="linechart-loader">
-              <SyncLoader color="#2d46b9" />
-            </div>
-          )}
-          {/* <Col lg={4}>
+      <div className="Tabs">
+        {/* Tab nav */}
+        <ul className="nav">
+          <li
+            className={activeTab === "Individual" ? "active" : ""}
+            onClick={handleTab1}
+          >
+            INDIVIDUAL
+          </li>
+          <li
+            className={activeTab === "Compare" ? "active" : ""}
+            onClick={handleTab2}
+          >
+            COMPARE
+          </li>
+        </ul>
+        <div className="outlet">
+          {activeTab === "Individual" ? (
+            <Container className="map-container">
+              <Row className="comparehead">
+                <h1>LINE GRAPH</h1>
+              </Row>
+              <Row xs={1}>
+                <Col>
+                  <SelectSearch
+                    options={options}
+                    placeholder="Choose your river"
+                    search
+                    filterOptions={fuzzySearch}
+                    onChange={handleChange}
+                    value={river}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                {riverData ? (
+                  <div className="graph-placeholder" id="graphplaceholder">
+                    <Plot
+                      data={[riverData.trace1, riverData.trace2]}
+                      layout={{
+                        title: { text: `River ${river}` },
+                        autosize: true,
+                        xaxis: {
+                          title: "Year",
+                        },
+                        yaxis: {
+                          title: "WQI",
+                        },
+                      }}
+                      useResizeHandler
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                  </div>
+                ) : (
+                  <div className="linechart-loader">
+                    <SyncLoader color="cyan" />
+                  </div>
+                )}
+                {/* <Col lg={4}>
             <div className="info-container"></div>
           </Col> */}
-        </Row>
-      </Container>
-
-      {/* <h1 className="comparehead">Compare Two rivers</h1>
-
-      <Container className="map-container">
-        <Row xs={2}>
-          <Col>
-            <SelectSearch
-              options={options}
-              placeholder="Choose a river"
-              search
-              filterOptions={fuzzySearch}
-              onChange={handleChangefirst}
-              value={firstriver}
-            />
-          </Col>
-          <Col>
-            <SelectSearch
-              options={options}
-              placeholder="Choose another river"
-              search
-              filterOptions={fuzzySearch}
-              onChange={handleChangesecond}
-              value={secondriver}
-            />
-          </Col>
-        </Row>
-        <Row xs={1} md={2}>
-          <Col lg={8}>
-            <div className="graph-placeholder" id="graphplaceholder">
-              <Plot
-                data={[comparedata.trace1,comparedata.trace2]}
-                layout={{ width: 700, height: 500}}
-              />
-            </div>
-          </Col>
-        </Row>
-      </Container> */}
+              </Row>
+            </Container>
+          ) : (
+            <Container className="map-container">
+              <Row className="comparehead">
+              <h1>COMPARE TWO RIVERS</h1>
+              </Row>
+              <Row xs={2}>
+                <Col>
+                  <SelectSearch
+                    options={options}
+                    placeholder="Choose a river"
+                    search
+                    filterOptions={fuzzySearch}
+                    onChange={handleChangefirst}
+                    value={firstriver}
+                  />
+                </Col>
+                <Col>
+                  <SelectSearch
+                    options={options}
+                    placeholder="Choose another river"
+                    search
+                    filterOptions={fuzzySearch}
+                    onChange={handleChangesecond}
+                    value={secondriver}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                  {comparedata ? (
+                    <div
+                      className="graph-placeholder"
+                    >
+                      <Plot
+                        data={[comparedata.trace1, comparedata.trace2]}
+                        layout={{
+                          autosize: true,
+                          xaxis: {
+                            title: "Year",
+                          },
+                          yaxis: {
+                            title: "WQI",
+                          },
+                        }}
+                        useResizeHandler
+                        style={{ width: "100%", height: "100%" }}
+                      />
+                    </div>
+                  ) : (
+                    <div className="linechart-loader">
+                      <SyncLoader color="#2d46b9" />
+                    </div>
+                  )}
+              </Row>
+            </Container>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
